db0133f860b471fe735abbe77289780ea60a7147
Shreyas Mavle <shreyasmavle007@gmail.com>
2020-06-02 22:29:07 +0530

    Added Queue to multiprocess

--- distance_using_postgres/load_testing/cube_testing_with_mp_module.py
+++ distance_using_postgres/load_testing/cube_testing_with_mp_module.py
@@ -6,6 +6,7 @@ import random
 # Third party library
 import psycopg2
 
+
 def sample_floats(low, high, k=1):
     """ Return a k-length list of unique random floats
         in the range of low <= x <= high
@@ -21,7 +22,7 @@ def sample_floats(low, high, k=1):
     return result
 
 
-def connect(i):
+def connect(i, queue):
     y = sample_floats(-2.00, 2.00, k=128)
     conn = psycopg2.connect(database="euclidean", user="postgres",
                             password="Narrator@027", host='127.0.0.1')
@@ -40,30 +41,38 @@ def connect(i):
     result = cur.fetchall()
     t2 = time.perf_counter()
 
-    print('Completed process Fetched {} results from query in {} seconds!'.format(
-        len(result), (t2 - t1)))
+    execution_time = round((t2 - t1), 2)
+
+    print('Completed process {}. Fetched {} results from query in {} seconds!'.format(i,
+                                                                                      len(result), execution_time))
 
     if conn is not None:
         cur.close()
         conn.close()
 
+    queue.put(execution_time)
+
 
 if __name__ == '__main__':
-
+    avg_time = list()
     processes = []
     input_processes = int(input('Enter number of processes to spawn : '))
-
+    queue = multiprocessing.Queue()
     start_time = time.time()
 
     for i in range(input_processes + 1):
-        process = multiprocessing.Process(target=connect, args=(i, ))
+        process = multiprocessing.Process(target=connect, args=(i, queue))
         processes.append(process)
         process.start()
 
-    x = [x.join() for x in processes]
+    for process in processes:
+        avg_time.append(queue.get())
+        process.join()
+
     finish_time = time.time()
 
     print(
-        f"Finished in {round((finish_time - start_time), 2)} second(s) using Multiprocessing")
+        f"Average time for each request = {round(sum(avg_time) / len(avg_time), 2)} second(s)")
+
     print(
-        f'Average time for each request = {round((finish_time - start_time), 2) / input_processes} second(s)')
+        f"Finished in {round((finish_time - start_time), 2)} second(s) using Multiprocessing")
